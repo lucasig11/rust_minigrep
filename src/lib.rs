53704b2574db2d80460bc7d34cfdeed2815e6eca
lucas <lucasfonsecard@icloud.com>
2021-06-27 17:03:47 -0300

    feat: add case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,13 @@ impl Program {
     pub fn run(args: &[String]) -> Result<(), Box<dyn Error>> {
         let config = Config::new(&args)?;
         let file = FileReader::init(&config.filename)?;
-        for line in file.search(config.query) {
+        let searcher = if config.case_sensitive {
+            file.search(config.query)
+        } else {
+            file.case_insensitive_search(config.query)
+        };
+
+        for line in searcher {
             println!("{}", line);
         }
         Ok(())
@@ -17,12 +23,19 @@ impl Program {
 struct Config<'a> {
     query: &'a str,
     filename: &'a str,
+    case_sensitive: bool,
 }
 
 impl<'a> Config<'a> {
     fn new(args: &'a [String]) -> Result<Self, &str> {
+        let case_sensitive = std::env::var("CASE_INSENSITIVE").is_err();
+
         match args.get(1..) {
-            Some([query, filename, ..]) => Ok(Self { query, filename }),
+            Some([query, filename, ..]) => Ok(Self {
+                query,
+                filename,
+                case_sensitive,
+            }),
             _ => Err("Missing arguments. Usage: minigrep <pattern> <file>"),
         }
     }
@@ -49,6 +62,16 @@ impl FileReader {
         }
         result
     }
+
+    fn case_insensitive_search(&self, pat: &str) -> Vec<&str> {
+        let mut result: Vec<&str> = Vec::new();
+        for line in self.contents.lines() {
+            if line.to_lowercase().contains(&pat.to_lowercase()) {
+                result.push(line.trim());
+            }
+        }
+        result
+    }
 }
 
 #[cfg(test)]
@@ -67,6 +90,26 @@ mod tests {
         assert_eq!(
             vec!["/home/user/.cargo/bin/cargo", "/home/user/.cargo/bin/rustc"],
             FileReader::search(
+                &FileReader {
+                    contents: contents.to_string()
+                },
+                query
+            )
+        )
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "uSeR";
+        let contents = "
+            /usr/bin/aws
+            /home/user/.cargo/bin/cargo
+            /home/user/.cargo/bin/rustc
+        ";
+
+        assert_eq!(
+            vec!["/home/user/.cargo/bin/cargo", "/home/user/.cargo/bin/rustc"],
+            FileReader::case_insensitive_search(
                 &FileReader {
                     contents: contents.to_string()
                 },
