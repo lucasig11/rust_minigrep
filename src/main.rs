fceb2ef5a279b0cdd289981c0f1578895ad2857c
lucas <lucasfonsecard@icloud.com>
2021-06-28 01:07:58 -0300

    chore: change println! to eprintln!

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if let Err(e) = Program::run(&args) {
-        println!("{}", e);
+        eprintln!("{}", e);
         process::exit(1);
     }
 }
